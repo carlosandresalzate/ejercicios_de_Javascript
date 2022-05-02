1c5f0e232c23079b295b58ae8cbdf8e2192bbbd3
carlos andres alzate <unarteman@gmail.com>
2022-05-02 18:29:17 -0300

    solucion al primer ejercicio!

---
+++ ejercicioUno.js
@@ -0,0 +1,11 @@
+
+/**
+ * @author Carlos Andres
+ */
+
+let myString = "Carlos Andres!";
+
+for (let i = 0; i < myString.length; i++) {
+    console.log(`En el indice ${i} posición ${i + 1} es igual a la letra ${myString[i]}`)
+}
+ 
