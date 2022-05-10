57dbc8f90842df2c4b5aa380c5a43d56fd25dc6c
carlos andres alzate <unarteman@gmail.com>
2022-05-10 13:09:38 -0300

    agrego solucion del ejercicio N° 5

---
+++ ejercicio_5.js
@@ -0,0 +1,10 @@
+let arrayUno = [1, "casa", {}];
+let arrayDos = [function(){}, true]
+
+function transformarATipos(param1) {
+    let newArray = param1.map((val) => typeof val);
+    console.log(newArray);
+}
+
+transformarATipos(arrayUno);
+transformarATipos(arrayDos);
