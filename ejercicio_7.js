1b0a31fb21e87eec446b9e70d6f2c1e37091470f
carlos andres alzate <unarteman@gmail.com>
2022-05-12 16:24:20 -0300

    agrego resolucion del ejercicio N° 7

---
+++ ejercicio_7.js
@@ -0,0 +1,18 @@
+/**
+ * @author Carlos Andres
+ */
+
+let myObject1 = {a: "h", b: "o", c: "l", d: "a"};
+let myObject2 = {z: "sa", x: "ra", y: "sa"};
+
+function concatenarObj(param1) {
+    let salida;
+    for(val in param1) {
+        salida = Object.values(param1).join('');
+    }
+    return salida;
+}
+
+console.log(concatenarObj(myObject1));
+console.log(concatenarObj(myObject2));
+
