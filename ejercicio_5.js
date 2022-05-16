5812e9d0eaa7a793007638b53a61797d5739344c
carlos andres alzate <unarteman@gmail.com>
2022-05-16 17:07:01 -0300

    agrego solucion usando forEach

--- ejercicio_5.js
+++ ejercicio_5.js
@@ -6,8 +6,21 @@ let arrayUno = [1, "casa", {}];
 let arrayDos = [function(){}, true]
 
 function transformarATipos(param1) {
+
+    // usando map
+/* 
     let newArray = param1.map((val) => typeof val);
     console.log(newArray);
+     */
+
+    // usando forEach
+    let nuevoArray = new Array();
+    param1.forEach((valor, indice, array) => {
+        nuevoArray.push(typeof valor);
+    });
+
+    console.log(nuevoArray);
+
 }
 
 transformarATipos(arrayUno);
