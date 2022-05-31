c066ea8d57e7b00ed49e12689318ef9832de7160
carlos andres alzate <unarteman@gmail.com>
2022-05-31 16:22:11 -0300

    retificando y modificando codigo anidado en else: antes devolvia lo que tenia el array, ahora devuelve propObj

--- ejercicio_10.js
+++ ejercicio_10.js
@@ -15,9 +15,9 @@ const arrDeFiltros3 = ['a', 'b', 'c', 'z'];
 
 function filtrarKeys(obj, array, exclusion = false) {
     let salida;
+    let propObj = Object.keys(obj);
     
     if (exclusion === true) {
-        let propObj = Object.keys(obj);
         salida = propObj.filter(val => {
             let filtro = array.indexOf(val);
             if(filtro === -1){
@@ -25,8 +25,8 @@ function filtrarKeys(obj, array, exclusion = false) {
             }
         });
     } else {
-        salida = array.filter(val => {
-            let filtro = obj.hasOwnProperty(val) === true;
+        salida = propObj.filter(val => {
+            let filtro = array.indexOf(val) !== -1;
             return filtro;
         });
     }
@@ -40,10 +40,10 @@ console.log('exclusion', filtrarKeys({ "a":1, "b":2, "c":3 }, ["a", "c"], true))
 console.log('exclusion', filtrarKeys({ "a":1, "b":2, "c":3 }, ["a", "b", "c"], true));
 console.log('exclusion', filtrarKeys({ "a":1, "b":2, "c":3 }, ["c"], true));
 console.log('inclusion', filtrarKeys({ "a":1, "b":2, "c":3 }, ["c"]));
-
-// console.log('inclusion:', filtrarKeys(objNum, arrDeFiltros1));
-// console.log('exclusion:',filtrarKeys(objNum, arrDeFiltros1, true));
-// console.log('inclusion:', filtrarKeys(objNum, arrDeFiltros2));
-// console.log('exclusion:',filtrarKeys(objNum, arrDeFiltros2, true));
-// console.log('inclusion:', filtrarKeys(objNum, arrDeFiltros3));
-// console.log('exclusion:',filtrarKeys(objNum, arrDeFiltros3, true));
+console.log("----");
+console.log('inclusion:', filtrarKeys(objNum, arrDeFiltros1));
+console.log('exclusion:',filtrarKeys(objNum, arrDeFiltros1, true));
+console.log('inclusion:', filtrarKeys(objNum, arrDeFiltros2));
+console.log('exclusion:',filtrarKeys(objNum, arrDeFiltros2, true));
+console.log('inclusion:', filtrarKeys(objNum, arrDeFiltros3));
+console.log('exclusion:',filtrarKeys(objNum, arrDeFiltros3, true));
