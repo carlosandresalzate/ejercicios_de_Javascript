3821518124153f9391a424ce137517c908940db9
Carlos Andres Alzate <unarteman@gmail.com>
2023-09-04 17:50:09 -0300

    cambio if anidado por ternario, es un poco mas limpio

--- mis_soluciones/ejercicio_10.js
+++ mis_soluciones/ejercicio_10.js
@@ -3,9 +3,9 @@
  */
 
 const objNum = {
-  a: 1,
-  b: 2,
-  c: 3
+	a: 1,
+	b: 2,
+	c: 3
 }
 
 const arrDeFiltros1 = ['a', 'd']
@@ -14,31 +14,27 @@ const arrDeFiltros3 = ['a', 'b', 'c', 'z']
 
 /* space-before-function-paren: off */
 function filtrarKeys(obj, array, exclusion = false) {
-  let salida
-  const propObj = Object.keys(obj)
+	let salida
+	const propObj = Object.keys(obj)
 
-  if (exclusion === true) {
-    salida = propObj.filter((val) => {
-      const filtro = array.indexOf(val)
-      if (filtro === -1) {
-        return val
-      }
-      return salida
-    })
-  } else {
-    salida = propObj.filter((val) => {
-      const filtro = array.indexOf(val) !== -1
-      return filtro
-    })
-  }
-
-  return salida
+	if (exclusion === true) {
+		salida = propObj.filter((val) => {
+			const filtro = array.indexOf(val)
+			return filtro === -1 ? val : salida
+		})
+	} else {
+		salida = propObj.filter((val) => {
+			const filtro = array.indexOf(val) !== -1
+			return filtro
+		})
+	}
+	return salida
 }
 
 console.log('exclusion', filtrarKeys({ a: 1, b: 2, c: 3 }, ['a', 'c'], true))
 console.log(
-  'exclusion',
-  filtrarKeys({ a: 1, b: 2, c: 3 }, ['a', 'b', 'c'], true)
+	'exclusion',
+	filtrarKeys({ a: 1, b: 2, c: 3 }, ['a', 'b', 'c'], true)
 )
 console.log('exclusion', filtrarKeys({ a: 1, b: 2, c: 3 }, ['c'], true))
 console.log('inclusion', filtrarKeys({ a: 1, b: 2, c: 3 }, ['c']))
